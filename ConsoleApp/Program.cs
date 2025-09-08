0d57dbc499d248a6b600d680031d122c0e9d735c
Christina Darmopuk <hristadarmopuk@gmail.com>
2025-09-08 12:05:41 +0300

    Calculation of the sum of numbers and output of the result have been added

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,7 +6,7 @@
         {
             int x = 5;
             int y = 10;
-            Console.WriteLine("5 + 10 = {}", x + y);
+            Console.WriteLine("5 + 10 = {0}", x + y);
             Console.ReadKey();
         }
     }
